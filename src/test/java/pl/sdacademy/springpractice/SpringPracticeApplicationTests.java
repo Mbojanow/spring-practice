b19d7d7484fd41ba3dadedf4342b1f0cf9c03a05
Kursant SDA <kursant@sda.pl>
2022-04-01 20:33:27 +0200

    Initial project

---
+++ src/test/java/pl/sdacademy/springpractice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.springpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
